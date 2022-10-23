15100deaaf7595c0a1d22af3ab370958674b5da0
aurumbright <aurum.elean@gmail.com>
2022-10-22 18:30:41 -0700

    added controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,61 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // GET all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // GET a single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtID })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that id!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // POST a new thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  // PUT / update a thought based on id
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtID },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // DELETE a thought
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtID })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that id!" })
+          : res.json({ message: "Thought deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
